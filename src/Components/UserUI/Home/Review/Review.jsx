d895debaf4439029e68d05c0867b84115ad4dbf1
md-mh <mmahedi49@gmail.com>
2022-01-03 18:17:26 +0600

    Review added

--- src/Components/UserUI/Home/Review/Review.jsx
+++ src/Components/UserUI/Home/Review/Review.jsx
@@ -13,38 +13,44 @@ const Review = () => {
   }, []);
 
   return (
-    <Container className="my-5">
-      <h1 className="text-center py-5 heading">
-        <span className="span">
-          Customer <span className="text-primary">Reviews</span>
-        </span>
-      </h1>
-      <Carousel variant="dark" indicators="false">
-        {
-          // mappimg data from reviews
-          reviews.map((service) => (
-            <Carousel.Item key={service._id} service={service}>
-              <Row className="justify-content-center">
-                <Col md={7}>
-                  <div className="text-center">
-                    <Rating
-                      className="p-2"
-                      initialRating={service.rating}
-                      fullSymbol={<AiFillStar />}
-                      emptySymbol={<AiOutlineStar />}
-                      readonly
-                    />
-                    <p>{service.comment}</p>
-                    <h4 className="text-end"> - {service.name}</h4>
-                    <br />
-                    <br />
-                  </div>
-                </Col>
-              </Row>
-            </Carousel.Item>
-          ))
-        }
-      </Carousel>
+    <Container className="my-5 px-5">
+      <Row>
+        <Col md={5}>
+          <h1 className="text-center py-5 heading">
+            <span className="span">
+              Customer <span className="text-primary">Reviews</span>
+            </span>
+          </h1>
+        </Col>
+        <Col md={7}>
+          <Carousel variant="dark" indicators="false">
+            {
+              // mappimg data from reviews
+              reviews.map((service) => (
+                <Carousel.Item key={service._id} service={service}>
+                  <Row className="justify-content-center">
+                    <Col md={7}>
+                      <div className="text-center">
+                        <Rating
+                          className="p-2"
+                          initialRating={service.rating}
+                          fullSymbol={<AiFillStar />}
+                          emptySymbol={<AiOutlineStar />}
+                          readonly
+                        />
+                        <p>{service.comment}</p>
+                        <h4 className="text-end"> - {service.name}</h4>
+                        <br />
+                        <br />
+                      </div>
+                    </Col>
+                  </Row>
+                </Carousel.Item>
+              ))
+            }
+          </Carousel>
+        </Col>
+      </Row>
     </Container>
   );
 };
